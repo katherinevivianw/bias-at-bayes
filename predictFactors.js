33ad06e51d7dd7a332a2e65d73beb01416cf842c
katherinevivianw <katherinevivianw@gmail.com>
2023-12-06 20:52:11 -0800

    Clean up code

--- predictFactors.js
+++ predictFactors.js
@@ -2,15 +2,24 @@ const OLD_KEYS = ["Gender", "OverTime", "JobSatisfaction", "MonthlyIncome", "Att
 const KEYS = ["Female", "OverTime", "JobSatisfaction", "MonthlyIncome", "Attrition"];
 
 function getMedianFactor(data, factor) {
+    /**
+     * Computes the median factor in the dataset.
+     */
     const factors = data.map(row => parseInt(row[factor]));
     return median(factors);
 }
 
 function deleteIrrelevantVariables(data) {
+    /**
+     * Deletes variables we are not considering from the data dictionary.
+     */
     return data.map(row => Object.fromEntries(OLD_KEYS.map(k => [k, row[k]])));
 }
 
 function convertDataToBoolean(data, medianIncome, medianSatisfaction) {
+    /**
+     * Converts all values in the data dictionary to be represented by boolean/Bernoulli values.
+     */
     return data.map(row => {
         const newRow = {};
 
@@ -33,12 +42,20 @@ function convertDataToBoolean(data, medianIncome, medianSatisfaction) {
 }
 
 function median(arr) {
+    /**
+     * Computes the median given an array of numbers. 
+     */
     const sortedArr = arr.slice().sort((a, b) => a - b);
     const mid = Math.floor(sortedArr.length / 2);
     return sortedArr.length % 2 !== 0 ? sortedArr[mid] : (sortedArr[mid - 1] + sortedArr[mid]) / 2;
 }
 
 async function load(filename) {
+    /**
+     * Loads the input file into a list of dictionaries, where each dictionary in the list corresponds
+     * to a row in the dataset. In each dictionary, the key corresponds to the column header name, and 
+     * the value corresponds to the numerical boolean value. 
+     */
     try {
         const response = await fetch(filename);
         if (!response.ok) {
@@ -66,11 +83,17 @@ async function load(filename) {
         return booleanData;
     } catch (error) {
         console.error(error);
-        // Handle the error as needed
     }
 }
 
 function getPXGivenY(xColumn, yColumn, data) {
+    /**
+     * Computes P(X = 1 | Y = 1) and P(X = 1 | Y = 0), where X is a single feature (column).
+     * x_column: name of the column containing the feature X.
+     * y_column: name of the class containing the class label.
+
+     * return: [P(X = 1 | Y = 0), P(X = 1 | Y = 1)]
+     */
     let xOneYOne = 0, xOneYZero = 0, yOne = 0, yZero = 0;
 
     for (const entry of data) {
@@ -92,6 +115,11 @@ function getPXGivenY(xColumn, yColumn, data) {
 }
 
 function getAllPXGivenY(yColumn, data) {
+    /**
+     * Computes P(X | Y) for all possible X and Y values. Returns probabilities in the form
+     * of a dictionary. The keys represent each possibly x value. The value of each key is a
+     * list of length 2, corresponding to [P(x_i | Y=0), P(x_i | Y=1)].
+     */
     const allPXGivenY = {};
     
     for (const xVal in data[0]) {
@@ -104,6 +132,9 @@ function getAllPXGivenY(yColumn, data) {
 }
 
 function getPY(yColumn, data) {
+    /**
+     * Computes P(Y = 1).
+     */
     let yOne = 0;
 
     for (const entry of data) {
@@ -116,6 +147,9 @@ function getPY(yColumn, data) {
 }
 
 function jointProb(xRow, yColumn, allPXGivenY, pY) {
+    /**
+     * Computes the joint probability of a single row and y.
+     */
     let probYGivenX = pY;
 
     for (const xVal in xRow) {
@@ -132,54 +166,46 @@ function jointProb(xRow, yColumn, allPXGivenY, pY) {
 }
 
 function getProbYGivenX(xRow, y, allPXGivenY, pY) {
+    /**
+     * Computes the probability of y given a single row under the 
+     * Naive Bayes assumption.
+     */
     return jointProb(xRow, y, allPXGivenY, pY);
 }
 
-function getUserXVals(xVal) {
-    if (xVal === KEYS[0]) { // Female case
-        return parseInt(readlineSync.question("Enter 1 or 0 for female. "));
-    } else if (xVal === KEYS[1]) { // OverTime case
-        return parseInt(readlineSync.question("Enter 1 or 0 for overtime. "));
-    } else if (xVal === KEYS[2]) { // JobSatisfaction case
-        return parseInt(readlineSync.question("Enter 1 or 0 for job satisfaction. "));
-    } else if (xVal === KEYS[3]) { // MonthlyIncome case
-        return parseInt(readlineSync.question("Enter 1 or 0 for monthly income. "));
-    } else { // Attrition case
-        return parseInt(readlineSync.question("Enter 1 or 0 for attrition. "));
-    }
-}
-
 function getUserYVal() {
+    /**
+     * Gets the Y Val from the selected radio button.
+     */
     const selectedRadioButton = document.querySelector('input[name="answer"]:checked');
     return selectedRadioButton ? selectedRadioButton.value : null;
 }
 
 function getUserXVals(yColumn) {
+    /**
+     * Gets user inputs from the checkboxes and returns inputs as a dictionary.
+     */
     const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
     const userInputs = {};
 
     checkboxes.forEach((checkbox) => {
-        userInputs[checkbox.name] = 1; // Set the value to 1 for checked checkboxes
+        userInputs[checkbox.name] = 1; // checked off checkboxes have a value of 1
     });
 
     KEYS.forEach(function(xVal) {
         if (!(xVal in userInputs)){
-            userInputs[xVal] = 0;   // Set x_vals that were not checked off to 0
+            userInputs[xVal] = 0;   // not checked off checkboxes have a value of 0
         }
     });
-    delete userInputs[yColumn];     // Delete y value we're predicting from the x value dict
+    delete userInputs[yColumn];     // delete y value we're predicting from the x value dict
 
     return userInputs;
 }
 
-function getUserInputs(yColumn) {
-    const userRow = getUserXVals(yColumn);
-    //userRow[yColumn] = 1; // Set the value to 1 for the selected radio button
-
-    return userRow;
-}
-
 function getUserRowOutput(userRow) {
+    /**
+     * Translates the userRow dict to a printable string in probability format.
+     */
     let output = "";
     for (const [key, value] of Object.entries(userRow)) {
         output = output.concat(", " + key + "=" + value);
@@ -189,6 +215,9 @@ function getUserRowOutput(userRow) {
 }
 
 function getUserRowWords(userRow) {
+    /**
+     * Translates the userRow dict to a printable string in word format.
+     */
     let output = "";
     for (const [key, value] of Object.entries(userRow)) {
         output = output.concat(", ")
@@ -199,27 +228,31 @@ function getUserRowWords(userRow) {
 } 
 
 function predictProbability(allPXGivenY, pY, data, yColumn) {
-    const userRow = getUserInputs(yColumn);
+    /**
+     * Calculates the final P(Y=1|X) and P(Y=0|X) probabilites and makes a prediction.
+     * Sends probabilities and prediction to the frontend to be displayable for user. 
+     */
+    const userRow = getUserXVals(yColumn);
     const userRowOutput = getUserRowOutput(userRow);
     const userRowWords = getUserRowWords(userRow);
 
-    // Get Probabilities
+    // get P(Y=1|X) and P(Y=0|X) probabilities
     const pOne = getProbYGivenX(userRow, 1, allPXGivenY, pY);
     const pZero = getProbYGivenX(userRow, 0, allPXGivenY, pY);
 
-    // Update the content of the HTML element with the id 'probability-result'
+    // sends probabilities to the frontned
     document.getElementById('probability-result-one').innerHTML = `Probability of ${yColumn} being true given <br> ${userRowWords}`;
     document.getElementById('probability-result-two').innerHTML = `= P(${yColumn}=1 | ${userRowOutput}) <br> = ${pOne}`;
     document.getElementById('probability-result-three').innerHTML = `Probability of ${yColumn} being false given <br> ${userRowWords}`;
     document.getElementById('probability-result-four').innerHTML = `= P(${yColumn}=0 | ${userRowOutput}) <br> = ${pZero}`;
 
-    const yBool = (pOne > pZero ? "true" : false);
-    // Update final classification prediction
-    document.getElementById('probability-result-five').innerHTML = `Final Prediction: ${yColumn} is ${yBool}`;
+    const yBool = (pOne > pZero ? "true" : false); // make final prediction based on which prob is greater
+    document.getElementById('probability-result-five').innerHTML = `Final Prediction: ${yColumn} is ${yBool}`;  // send to frontend
 
     return p;
 }
 
+// Run Naive Bayes algorithm on the specified dataset.
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('submitButton').addEventListener('click', async function () {
         try {
@@ -251,23 +284,23 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// Hides the corresponding checkbox when the val is selected for a y val
 document.addEventListener('DOMContentLoaded', function () {
-    // Add event listener to the radio buttons for immediate response
+    // add event listener to the radio buttons for immediate response
     const radioButtons = document.querySelectorAll('input[type="radio"][name="answer"]');
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
     const checkboxLabels = document.querySelectorAll('input[type="checkbox"] + label'); // Select labels associated with checkboxes
 
     radioButtons.forEach(radioButton => {
         radioButton.addEventListener('change', function () {
-            // Set visibility for corresponding checkbox and label based on selected radio button value
             const selectedValue = this.value;
             checkboxes.forEach((checkbox, index) => {
                 if (checkbox.name === selectedValue) {
-                    checkbox.style.visibility = 'hidden'; // Make the checkbox invisible
-                    checkboxLabels[index].style.visibility = 'hidden'; // Make the label next to the checkbox invisible
+                    checkbox.style.visibility = 'hidden'; // make the checkbox invisible
+                    checkboxLabels[index].style.visibility = 'hidden'; // make the label next to the checkbox invisible
                 } else {
-                    checkbox.style.visibility = 'visible'; // Make other checkboxes visible
-                    checkboxLabels[index].style.visibility = 'visible'; // Make other labels next to checkboxes visible
+                    checkbox.style.visibility = 'visible'; // make other checkboxes visible
+                    checkboxLabels[index].style.visibility = 'visible'; // make other labels next to checkboxes visible
                 }
             });
         });
